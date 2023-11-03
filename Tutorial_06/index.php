31a9f595dc820b1f310461248db73e99ec387af8
ayemoekyaw99 <ayemoekyaw83@gmail.com>
2023-11-03 23:03:59 +0630

    update tuotrial06

--- Tutorial_06/index.php
+++ Tutorial_06/index.php
@@ -13,7 +13,7 @@
       <div class="row mb-5">
         <div class="col-6 offset-2">
           <h5>Create Directory and Upload Files</h5>
-          <form action="upload.php" method="post" enctype="multipart/form-data">
+          <form action="" method="post" enctype="multipart/form-data">
             <label for="dirname">Folder Name:</label>
             <input type="text" name="dirname" id="dirname" class="form-control" required >
             <br>
@@ -27,18 +27,39 @@
     </div>
 </body>
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $dirname = $_POST["dirname"];
+    if (!file_exists($dirname)) {
+        mkdir($dirname, 0777, true);
+    }
+    if (isset($_FILES["file"])) {
+        $target_dir = $dirname . '/';
+        $target_file = $target_dir . basename($_FILES["file"]["name"]);
+
+        if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+            echo "File uploaded successfully.";
+        } else {
+            echo "Error uploading file.";
+        }
+    } else {
+        echo "No file selected for upload.";
+    }
+}
+?>          
+<?php
      $folders = scandir('./');
         foreach ($folders as $folder) {
           if (is_dir($folder) && $folder != '.' && $folder != '..' && $folder !='../' && $folder!='css' ) {
               $files = scandir($folder);
                   foreach ($files as $file) {
                       if (is_file("$folder/$file")) {
-                        echo "<span class='col-3 d-block mt-3 ms-5'>
+                        echo "<div class='col-6 offset-2'>
+                                <span class='col-3 d-block mt-3 ms-5'>
                                   <img src='$folder/$file' class='img-thumbnail d-inline' >
                                   <span class='d-inline'>$file</span>
-                                  <button class='btn btn-danger d-block col-12'>Delete</button>
-                                </span>";
-                        
+                                  <button class='btn btn-danger d-block col-12 mb-3'>Delete</button>
+                                </span>
+                              </div>";
                       }
                 }
               }
