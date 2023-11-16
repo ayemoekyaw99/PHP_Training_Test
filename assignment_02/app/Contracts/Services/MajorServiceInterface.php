2cca3c6f30651851a1f03bbafafb71ec84be5813
Aye Moe Kyaw <ayemoekyaw83@gmail.com>
2023-11-16 15:13:38 +0630

    add assignment02

---
+++ assignment_02/app/Contracts/Services/MajorServiceInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Contracts\Services;
+
+interface MajorServiceInterface
+{
+    public function getAllMajors();
+
+    public function create(array $data);
+
+    public function destroy(int $id);
+    public function getMajorById(int $id);
+
+    public function update(array $data, int $id);
+}
--- assignment_02/app/Providers/AppServiceProvider.php
+++ assignment_02/app/Providers/AppServiceProvider.php
@@ -11,8 +11,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+         // Major
+        $this->app->bind('App\Contracts\Services\MajorServiceInterface', 'App\Services\MajorService');
+        $this->app->bind('App\Contracts\Dao\MajorDaoInterface', 'App\Dao\MajorDao');
+
+        // // Student
+        $this->app->bind('App\Contracts\Services\StudentServiceInterface', 'App\Services\StudentService');
+        $this->app->bind('App\Contracts\Dao\StudentDaoInterface', 'App\Dao\StudentDao');
     }
+    
 
     /**
      * Bootstrap any application services.
