fc6e68838e9fb7e5927aea35c3504cf5a91f5492
Aye Moe Kyaw <ayemoekyaw83@gmail.com>
2023-11-20 22:03:54 +0630

    update PR review fix

--- first_task_app/app/Http/Controllers/TaskController.php
+++ first_task_app/app/Http/Controllers/TaskController.php
@@ -5,24 +5,30 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Task;
+
 class TaskController extends Controller
 {
-
-    public function index(){
+    public function index()
+    {
         $tasks = Task::orderBy('created_at', 'asc')->get();
-         return view('tasks', [
-        'tasks' => $tasks
-    ]);
+        return view('tasks', [
+            'tasks' => $tasks
+        ]);
     }
-   public function create(Request $request){
-     Validator::make($request->all(),['name' => 'required|max:255',
+
+    public function create(Request $request)
+    {
+        Validator::make($request->all(), [
+            'name' => 'required|max:255',
         ])->validate();
-       $data=['name'=>$request->name];
-       Task::create($data);
-       return redirect()->route('task')->with(['status'=>'Successful created']);
-   }
-   public function delete(Request $request,$id){
-    Task::find($id)->delete();
-    return redirect()->route('task')->with(['status'=>'Successful deleted']);
-   }
+        $data = ['name' => $request->name];
+        Task::create($data);
+        return redirect()->route('task')->with(['status' => 'Successful created']);
+    }
+
+    public function delete(Request $request, $id)
+    {
+        Task::find($id)->delete();
+        return redirect()->route('task')->with(['status' => 'Successful deleted']);
+    }
 }
